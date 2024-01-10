3287f386ad6078f6e8e77fc35272f7caa3212048
AndrewValentini <avalentini@carthage.edu>
2024-01-09 19:52:07 -0600

    Tried another example

--- .ipynb_checkpoints/definitions-checkpoint.py
+++ .ipynb_checkpoints/definitions-checkpoint.py
@@ -2,7 +2,6 @@ from sympy import *
 
 def christoffel_symbols(metric_tensor, inverse_metric_tensor, coordinates):
     num_coords = len(coordinates)
-    # Creating a rank 3 tensor(3D matrix) where all of the Christoffel symbols will be stored
     christoffel = [[[0 for _ in range(num_coords)] for _ in range(num_coords)] for _ in range(num_coords)]
 
     for i in range(num_coords):
@@ -20,10 +19,8 @@ def christoffel_symbols(metric_tensor, inverse_metric_tensor, coordinates):
 
     return christoffel
 
-
 def Riemann(metric_tensor, inverse_metric_tensor, coordinates):
     num_coordinates = len(coordinates)
-    # Creating a 4D matrix to store the Riemann curvature tensor values in
     riemann = [[[[0 for _ in range(num_coordinates)] for _ in range(num_coordinates)] for _ in range(num_coordinates)] for _ in range(num_coordinates)]
 
     for i in range(num_coordinates):
@@ -32,20 +29,68 @@ def Riemann(metric_tensor, inverse_metric_tensor, coordinates):
                 for l in range(num_coordinates):
                     sum_term = 0
                     for m in range(num_coordinates):
-                        term1 = .5* inverse_metric_tensor[i,m] * (
-                            diff(diff(metric_tensor[m,l], coordinates[j]), coordinates[k]) -
-                            diff(diff(metric_tensor[m,k], coordinates[j]), coordinates[l]) +
-                            diff(diff(metric_tensor[j,k], coordinates[m]), coordinates[l]) -
-                            diff(diff(metric_tensor[j,l], coordinates[m]), coordinates[k])
+                        term1 = .5 * inverse_metric_tensor[i, m] * (
+                            diff(diff(metric_tensor[m, l], coordinates[j]), coordinates[k]) -
+                            diff(diff(metric_tensor[m, k], coordinates[j]), coordinates[l]) +
+                            diff(diff(metric_tensor[j, k], coordinates[m]), coordinates[l]) -
+                            diff(diff(metric_tensor[j, l], coordinates[m]), coordinates[k])
                         ) 
                         sum_term += term1
                     riemann[i][j][k][l] = sum_term
     return riemann
+    
+#Here, the Riemann tensor is only defined in terms of the Christoffel symbols since they seem to be computed correctly:
 
+def Riemann_from_christoffel(christoffel, coordinates):
+    num_coordinates = len(coordinates)
+    riemann = [[[[0 for _ in range(num_coordinates)] for _ in range(num_coordinates)] for _ in range(num_coordinates)] for _ in range(num_coordinates)]
 
+    for i in range(num_coordinates):
+        for j in range(num_coordinates):
+            for k in range(num_coordinates):
+                for l in range(num_coordinates):
+                    term1 = diff(christoffel[i][l][k], coordinates[j]) - diff(christoffel[i][k][j], coordinates[l])
+                    term2 = sum(christoffel[m][k][j] * christoffel[i][m][l] for m in range(num_coordinates))
+                    term3 = sum(christoffel[m][l][k] * christoffel[i][m][j] for m in range(num_coordinates))
+                    riemann[i][j][k][l] = term1 + term2 - term3
+
+    return riemann
+    
+# Below is old code that does not seem to work
+"""
+def Riemann_all_lower(metric_tensor, inverse_metric_tensor, coordinates):
+    num_coordinates = len(coordinates)
+    riemann_full = Riemann(metric_tensor, inverse_metric_tensor, coordinates)
+
+    riemann_lower = [[[[
+        sum(
+            riemann_full[i][m][j][k] * inverse_metric_tensor[m, l]
+            for m in range(num_coordinates)
+        )
+        for l in range(num_coordinates)
+    ] for k in range(num_coordinates)] for j in range(num_coordinates)] for i in range(num_coordinates)]
+
+    return riemann_lower
+"""
+
+def Riemann_all_lower(metric_tensor, inverse_metric_tensor, riemann_upper, coordinates):
+    num_coordinates = len(coordinates)
+    
+    # Compute Riemann curvature tensor with four lower indices
+    riemann_lower = [[[[
+        sum(
+            metric_tensor[i, m] * riemann_upper[m][j][k][l]
+            for m in range(num_coordinates)
+        )
+        for l in range(num_coordinates)
+    ] for k in range(num_coordinates)] for j in range(num_coordinates)] for i in range(num_coordinates)]
+
+    return riemann_lower
+
+                        
 def Ricci_tensor(metric_tensor, inverse_metric_tensor, coordinates):
     num_coordinates = len(coordinates)
-    ricci = [[0 for _ in range(num_coordinates)] for _ in range(num_coordinates)]
+    ricci_tensor = [[0 for _ in range(num_coordinates)] for _ in range(num_coordinates)]
 
     for i in range(num_coordinates):
         for j in range(num_coordinates):
@@ -60,11 +105,9 @@ def Ricci_tensor(metric_tensor, inverse_metric_tensor, coordinates):
                             diff(diff(metric_tensor[j, l], coordinates[m]), coordinates[k])
                         )
                         sum_term += term1
-            ricci[i][j] = sum_term
+            ricci_tensor[i,j] = sum_term
 
     return ricci_tensor
-
-
 
 def Ricci_scalar(metric_tensor, inverse_metric_tensor, coordinates):
     num_coordinates = len(coordinates)
@@ -87,9 +130,22 @@ def Ricci_scalar(metric_tensor, inverse_metric_tensor, coordinates):
 
     return ricci_scalar
 
+def Einstein_tensor(metric_tensor, inverse_metric_tensor, coordinates):
+    num_coordinates = len(coordinates)
+    
+    # Compute Ricci tensor
+    ricci_tensor = Ricci_tensor(metric_tensor, inverse_metric_tensor, coordinates)
+    
+    # Compute Ricci scalar
+    ricci_scalar = Ricci_scalar(metric_tensor, inverse_metric_tensor, coordinates)
 
+    # Compute Einstein tensor using the formula: G_{mu nu} = R_{mu nu} - (1/2) R g_{mu nu}
+    einstein_tensor = [[0 for _ in range(num_coordinates)] for _ in range(num_coordinates)]
+    for mu in range(num_coordinates):
+        for nu in range(num_coordinates):
+            einstein_tensor[mu,nu] = ricci_tensor[mu,nu] - (1/2) * ricci_scalar * metric_tensor[mu, nu]
 
-
+    return einstein_tensor
 
 
 
